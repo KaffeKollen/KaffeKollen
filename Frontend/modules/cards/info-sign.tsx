008fe7145bfd37a3132f0857c70401ca98ec799c
zaamme <57627333+zaamme@users.noreply.github.com>
2021-12-02 10:57:58 +0100

    updated modules

--- Frontend/modules/cards/info-sign.tsx
+++ Frontend/modules/cards/info-sign.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { CardTemplate } from './card-template.tsx';
 
 import AbbLogoPNG from '../../public/abb-logga.png';
-import AbbLogoSVG from '../../public/abb-logga.svg';
+import AbbLogoSVG from '/public/abb-logga.svg';
 
 export const InfoSign: React.FC = ({}) => {
 	return (
 		<CardTemplate>
 			<div className="grid-span-7">
-				<img src={AbbLogoPNG} alt="ABB Logo" />
-				<img src={AbbLogoSVG} alt="ABB Logo" />
+				<img src='/abb-logga.png' alt="ABB Logo" />
+				<img src='/abb-logga.svg' alt="ABB Logo" />
 				<div className="text-xl  font-bold">ABB</div>
 			</div>
 		</CardTemplate>
