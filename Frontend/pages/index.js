94448f565602a6f1e3d0322c2e2bec426bdf6c6a
Rasmus <54621803+abbrasris@users.noreply.github.com>
2021-12-10 09:25:16 +0100

    Move store file

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,24 +1,24 @@
 import "tailwindcss/tailwind.css";
-import './store';
+import "../utils/store";
 
 import { Header } from "../modules/header.tsx";
 import { Billboard } from "../modules/billboard.tsx";
 
 export default function Home() {
-	return (
-		<div className="min-h-screen w-screen bg-gray-100 flex justify-center content-center overflow-hidden">
-		{/* <div className="min-h-screen w-screen bg-gradient-to-br from-yellow-100 to-red-100 flex justify-center content-center overflow-hidden"> */}
-			<div className="w-320 md:max-w-6xl max-w-md min-w-base my-auto p-6">
-				<Header />
-				<Billboard />
-			</div>
+  return (
+    <div className="min-h-screen w-screen bg-gray-100 flex justify-center content-center overflow-hidden">
+      {/* <div className="min-h-screen w-screen bg-gradient-to-br from-yellow-100 to-red-100 flex justify-center content-center overflow-hidden"> */}
+      <div className="w-320 md:max-w-6xl max-w-md min-w-base my-auto p-6">
+        <Header />
+        <Billboard />
+      </div>
 
-			<style jsx global>{`
-				html,
-				body {
-					padding: 0;
-					margin: 0;
-				}
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+        }
 
         * {
           box-sizing: border-box;
---
+++ Frontend/utils/store.js
@@ -0,0 +1,48 @@
+module.exports = global.store = {
+  current_volume: 43,
+  lineChart: {
+    categories: [
+      "01 Dec",
+      "02 Dec",
+      "03 Dec",
+      "04 Dec",
+      "05 Dec",
+      "06 Dec",
+      "07 Dec",
+      "08 Dec",
+      "09 Dec",
+    ],
+    series: [
+      {
+        name: "Koppar kaffe",
+        data: [12, 13, 16, 9, 15, 14, 12, 11, 9],
+      },
+    ],
+  },
+  leaderboards: {
+    week: [
+      { name: "Person1 Person1 Person1 Person1 Person1Person1 ", score: "25" },
+      { name: "Person2", score: "18" },
+      { name: "Person3", score: "16" },
+      { name: "Person4", score: "16" },
+      { name: "Person5", score: "14" },
+      { name: "Person6", score: "13" },
+      { name: "Person7", score: "12" },
+      { name: "Person8", score: "9" },
+      { name: "Person9", score: "8" },
+      { name: "Person10", score: "7" },
+      { name: "Person11", score: "7" },
+      { name: "Person12", score: "5" },
+      { name: "Person13", score: "5" },
+      { name: "Person14", score: "5" },
+      { name: "Person15", score: "4" },
+      { name: "Person16", score: "4" },
+      { name: "Person17", score: "3" },
+      { name: "Person18", score: "2" },
+      { name: "Person19", score: "2" },
+      { name: "Person20", score: "2" },
+      { name: "Person21", score: "1" },
+      { name: "Person22", score: "1" },
+    ],
+  },
+};
